87c7b9f62c1bce93fa7c941deb8b37570c7f9944
pratapsinghmadhavendra <pratapsinghmadhavendra@gmail.com>
2021-01-04 20:23:19 +0530

    second commit

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins! Hurrah";
 }
 else {
-  document.querySelector("h1").innerHTML = "Draw!";
+  document.querySelector("h1").innerHTML = "Draw Work Hard!";
 }
